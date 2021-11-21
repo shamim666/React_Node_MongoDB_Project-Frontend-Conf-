f7cfec77c584199c1f0125162ab303ba11f47612
shamim <shamim.pstu@gmail.com>
2021-11-21 17:45:26 +0600

    updated

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Users = () => {
+
+const [users , setUsers] = useState([])
+
+
+useEffect( ()=>{
+
+    fetch('http://localhost:8000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+
+
+}, [])
+
+
+const handleDeleteUser = id =>{
+
+    const proceed = window.confirm('Are you sure want to delete ?')
+
+    if(proceed){
+
+        const url = `http://localhost:8000/users/${id}`
+        fetch(url,{
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data =>{
+            if(data.deletedCount > 0){
+                alert('deleted successfully')
+                const remainingUsers = users.filter(user => user._id !== id)
+                setUsers(remainingUsers);
+            }
+        })
+    }
+}
+
+
+
     return (
         <div>
-            <h2>This is Users</h2>
+            <h2>Users Available : {users.length} </h2>
+            <ul>
+                {
+                   users.map( user => <li key={user._id}>name:{user.name}; email: {user.email} 
+                   
+                   <button>update</button>
+                   <button onClick={() => handleDeleteUser(user._id)}>X</button>
+                   
+                   </li> ) 
+                }
+            </ul>
         </div>
     );
 };
